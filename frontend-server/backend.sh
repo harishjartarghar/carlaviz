2ab113a8426f454be813aded043dbbc46d3d359b
harishjartarghar <harish.jartarghar@gmail.com>
2020-11-11 00:30:14 +0530

    frontend-server added

---
+++ frontend-server/backend.sh
@@ -0,0 +1,6 @@
+echo "Make sure you have launched the carla server."
+echo "Launching backend."
+./carlaviz/backend/bin/backend ${CARLA_SERVER_IP} ${CARLA_SERVER_PORT} &
+sleep 5
+
+echo "Backend launched."
