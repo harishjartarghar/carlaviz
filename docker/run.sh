9a9138785cfe925cf996d33aeb58139b51ee7119
harishjartarghar <harish.jartarghar@gmail.com>
2020-11-11 09:32:05 +0530

    path changed

--- docker/run.sh
+++ docker/run.sh
@@ -14,6 +14,8 @@ echo -e "CARLAVIZ_HOST_IP=${CARLAVIZ_HOST_IP}" >> /home/carla/.env
 echo "Frontend server application launching"
 sleep 2
 cd ./frontend-server/
+npm install & 
+sleep 30
 node index.js &
 sleep 10
 echo "Frontend server application launched"
@@ -22,7 +24,7 @@ echo "Frontend server application launched"
 echo "Launching frontend"
 
 sleep 2
-cd ./frontend/
+cd ../frontend/
 yarn start &
 sleep 10
 echo "Frontend launched. Please open your browser"
