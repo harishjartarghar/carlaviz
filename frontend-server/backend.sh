86c66e90dd7200e4ee3fed3a8f8c79f2bb1f67ed
Harish Jartarghar <51698550+harishjartarghar@users.noreply.github.com>
2020-12-13 17:37:09 +0530

    Update backend.sh

--- frontend-server/backend.sh
+++ frontend-server/backend.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+
+echo "Launching example.py"
+cd /home/carla/carlaviz/examples  && python3 example.py
+
+
 echo "Make sure you have launched the carla server."	
 echo "Launching backend."
 cd /home/carla/carlaviz/backend/bin  && echo "directorysearch" &&  ls -R && ./backend ${CARLA_SERVER_IP} ${CARLA_SERVER_PORT}
