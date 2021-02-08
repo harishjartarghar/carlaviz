a199ca67dfd6215a9ada6e93c753b00f181f05f4
RVCECS6052 <harish.jartarghar@gmail.com>
2021-02-08 14:56:53 +0530

    backend path changed

--- frontend-server/backend.sh
+++ frontend-server/backend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Make sure you have launched the carla server."	
 echo "Launching backend."
-cd /home/carla/carlaviz/backend/bin  && echo "directorysearch" &&  ls -R && ./backend ${CARLA_SERVER_IP} ${CARLA_SERVER_PORT}
+cd ../backend/bin/  && echo "directorysearch" &&  ls -R && ./backend ${CARLA_SERVER_IP} ${CARLA_SERVER_PORT}
 sleep 5
 
 
